ce5840cc0c2936e1d3036ea79fefca9292f1ea5c
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-23 18:45:35 +1000

    first components creation

---
+++ README.md
@@ -0,0 +1,10 @@
+# Forms lesson goals
+
+Goals:
+- Form with React class components
+- Edit-in-place components
+- Component communications & lifting state up
+
+- Parent component stores data
+    - Display component, receives data as props and displays it
+    - Form component, receives data as props, and function as props, and displays a form and calls the received functions
---
+++ src/BusinessCardDisplay.jsx
@@ -0,0 +1,17 @@
+export default function BusinessCardDisplay(props){
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h3>{props.email}</h3>
+        </div>
+    )
+}
+
+// function BusinessCardDisplay({name, email}){
+//     return (
+//         <div>
+//             <h1>{name}</h1>
+//             <h3>{email}</h3>
+//         </div>
+//     )
+// }
