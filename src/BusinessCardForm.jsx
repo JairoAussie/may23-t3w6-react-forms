de0e958366db538807e8ca4844ff24d0cfd520eb
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-25 09:13:03 +1000

    add per-property validation

--- src/BusinessCardForm.jsx
+++ src/BusinessCardForm.jsx
@@ -1,13 +1,10 @@
 import { Component } from "react";
 
 export default class BusinessCardForm extends Component{
-    constructor(props){
-        super(props);
-    }
 
     handleChangeInput = (event) => {
-        console.log(event.target.name);
-        console.log(event.target.value);
+        // console.log(event.target.name);
+        // console.log(event.target.value);
         this.props.updateState(event.target.name, event.target.value);
     }
 
@@ -16,12 +13,12 @@ export default class BusinessCardForm extends Component{
         return(
             <form>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" id="nameInput" 
+                <input type="text" name="name" id="name" 
                 value={this.props.name}
                 onChange={this.handleChangeInput}
                 />
                 <label htmlFor="email">Email: </label>
-                <input type="email" name="email" id="emailInput" 
+                <input type="email" name="email" id="email" 
                 value={this.props.email}
                 onChange={this.handleChangeInput}
                 />
