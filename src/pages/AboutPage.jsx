8213acc7eaa7cf727f1206cf8445ef7998695695
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-25 10:33:22 +1000

    nested routing with Outlet in the route

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,7 @@
-import { Outlet } from "react-router-dom";
-
 export default function AboutPage (){
     return(
         <div>
             <h1>About the App</h1>
-
-            <Outlet />
         </div>
     )
 }
